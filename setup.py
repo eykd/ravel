8d3e36b4e1734ef4f2528a08ad0834717b8712d9
eykd <david.eyk@gmail.com>
2022-09-17 07:23:52 -0700

    Move source into src/, use setup.cfg

--- setup.py
+++ setup.py
@@ -1,32 +1,4 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-PROJECT_NAME = 'ravel'
-VERSION = '0.1.dev0'
-
-setup(
-    name = PROJECT_NAME,
-    version = VERSION,
-    author = "David Eyk",
-    author_email = "david@worldsenoughstudios.com",
-    modules = [PROJECT_NAME],
-    install_requires = [
-        'attrs',
-        'awesome-slugify',
-        'blinker',
-        'click',
-        'colorclass',
-        'marshmallow',
-        'parsimonious',
-        'path',
-        'pydash',
-        'straight.plugin',
-        'strictyaml',
-        'syml',
-    ],
-    entry_points = dict(
-        console_scripts = [
-            'ravel = ravel.cli:main',
-        ]
-    ),
-)
+setup()
